06e19dc947204b9da6e23869e28d2fa7e8e73ac0
Tim Cargan <timcargan@gmail.com>
2022-07-18 20:14:24 +0100

    Autoreg LSTM

--- src/chemise/layers/lstm.py
+++ src/chemise/layers/lstm.py
@@ -14,19 +14,32 @@ class SimpleLSTM(nn.Module):
         initial_state = SimpleLSTM.initialize_carry((batch_size,), self.hidden_size)
         _, forward_outputs = SimpleLSTM()(initial_state, inputs)
     """
+    cell: nn.OptimizedLSTMCell = None
+
     @functools.partial(
-      nn.transforms.scan,
-      variable_broadcast='params',
-      in_axes=1, out_axes=1,
-      split_rngs={'params': False})
+        nn.transforms.scan,
+        variable_broadcast='params',
+        in_axes=1, out_axes=1,
+        split_rngs={'params': False})
     @nn.compact
     def __call__(self, carry, x):
-        return nn.OptimizedLSTMCell()(carry, x)
-
-    @staticmethod
-    def initialize_carry(batch_dims, hidden_size):
-        # Use fixed random key since default state init fn is just zeros.
-        return nn.OptimizedLSTMCell.initialize_carry(
-            jax.random.PRNGKey(0), batch_dims, hidden_size)
+        carry, x = self.cell(carry, x)
+        return carry, x
 
 
+class AutoregLSTM(nn.Module):
+    output_layer: nn.Dense = None
+    cell: nn.OptimizedLSTMCell = None
+
+    @functools.partial(
+        nn.transforms.scan,
+        variable_broadcast='params',
+        in_axes=1, out_axes=1,
+        split_rngs={'params': False})
+    @nn.compact
+    def __call__(self, carry_pred, x):
+        carry, past_pred = carry_pred
+        in_x = jnp.concatenate([x, past_pred], axis=-1)
+        carry, y = self.cell(carry, in_x)
+        y = self.output_layer(y)
+        return (carry, y), y
