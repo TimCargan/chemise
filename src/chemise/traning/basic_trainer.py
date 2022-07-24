85a1c9740f7764a5d9e045b46bc318585d0fd63d
Tim Cargan <timcargan@gmail.com>
2022-07-24 17:05:52 +0100

    - replicate to number of GPUs

--- src/chemise/traning/basic_trainer.py
+++ src/chemise/traning/basic_trainer.py
@@ -127,7 +127,8 @@ class BasicTrainer:
         :return:
         """
         start_cb(self)
-        for batch in prefetch(data.batch(1, drop_remainder=True)):
+        reps = c if (c := jax.device_count("GPU")) > 1 else 1
+        for batch in prefetch(data.batch(reps, drop_remainder=True)):
             step_start_cb(self)
             r_state = replicate(self.state)
             r_state, metrics = step_fn(r_state, batch)
