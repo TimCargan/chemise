a9325b8a8104858018cbb47e44c9ce4b3aa1a6c9
Tim Cargan <timcargan@gmail.com>
2022-08-31 22:59:18 +0100

    Move prefetch out
    Signed-off-by: Tim Cargan <timcargan@gmail.com>

---
+++ src/chemise/traning/prefetch.py
@@ -0,0 +1,33 @@
+import queue
+from threading import Thread
+
+import jax
+
+
+class Prefetch(Thread):
+    """
+    Wrap an iterator with a thead to load and prefetch onto the GPU(s) in a no-blocking way
+    """
+    def __init__(self, data: iter,  buffer_size: int = 3):
+        super(Prefetch, self).__init__()
+        self.data = data
+        self.q = queue.Queue(buffer_size)
+
+    def run(self):
+        devices = jax.local_devices()
+
+        def _prefetch(xs):
+            return jax.device_put_sharded(list(xs), devices)
+
+        for data in self.data:
+            self.q.put(jax.tree_util.tree_map(_prefetch, data))
+        self.q.put(None)
+
+    def __iter__(self):
+        self.start()
+        return self
+
+    def __next__(self):
+        if data := self.q.get():
+            return data
+        raise StopIteration
