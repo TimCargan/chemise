d4d89cc32f4fb62b9a137edadc1f153224cdc88b
Tim Cargan <timcargan@gmail.com>
2022-09-02 15:48:26 +0100

    Add layer_norm to MLC
    Signed-off-by: Tim Cargan <timcargan@gmail.com>

--- src/chemise/layers/mlp.py
+++ src/chemise/layers/mlp.py
@@ -1,6 +1,7 @@
 from typing import Callable, Sequence
 
 import flax.linen as nn
+import jax.numpy as jnp
 from jaxtyping import n
 
 
@@ -34,15 +35,19 @@ class MLC(nn.Module):
     features: int
     kernel_size: Sequence[int] = (3, 3)
     pool_size: Sequence[int] = (2, 2)
-    activation: Callable[[n], n] = nn.relu
+    pool_fn: Callable[[n], n] = nn.max_pool
+    activation_fn: Callable[[n], n] = nn.relu
     key: str = None
     @nn.compact
     def __call__(self, x: n["... H W C"]) -> n:
         if self.key:
             x = x[self.key]
 
+        axes = jnp.arange(-1, len(self.kernel_size) * -1)
+
         for d in range(self.depth):
             x = nn.Conv(features=self.features, kernel_size=self.kernel_size, padding="SAME")(x)
-            x = nn.max_pool(x, self.pool_size, strides=self.pool_size)
-            x = self.activation(x)
+            x = self.pool_fn(x, self.pool_size, strides=self.pool_size)
+            x = self.activation_fn(x)
+            x = nn.LayerNorm(feature_axes=axes, reduction_axes=axes)(x)
         return x
