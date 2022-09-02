96cde8ab56e9c7ce902f5780c1345a8a01e189fe
Tim Cargan <timcargan@gmail.com>
2022-09-02 17:40:01 +0100

    Clean up tests and add a JIT call
    Signed-off-by: Tim Cargan <timcargan@gmail.com>

--- src/chemise/layers/test_mlp.py
+++ src/chemise/layers/test_mlp.py
@@ -1,3 +1,4 @@
+import chex
 import jax
 import jax.test_util
 import numpy as np
@@ -13,33 +14,29 @@ jax.config.parse_flags_with_absl()
 Tests for the liner Multi Layer (ML) Models
 for the most part we just check that the shapes are correct
 """
-class MLPTest(parameterized.TestCase):
-    @parameterized.parameters(
-        (1, 128, 16, 128),
-        (3, 128, 16, 128),
-        (0, 128, 16, 16)
+class MLPTest(chex.TestCase):
+    @chex.variants(with_jit=True, without_jit=True)
+    @parameterized.product(
+        (dict(depth=1, width=128, in_size=16, exp_width=128),
+         dict(depth=3, width=64, in_size=16, exp_width=64),
+         dict(depth=0, width=128, in_size=16, exp_width=16)
+         ),
+        dims=(1,2,3)
     )
-    def test_mlp_2d_returns_correct_output_shape(self, depth, width, in_size, exp_width):
+    def test_returns_correct_output_shape(self, depth, width, in_size, exp_width, dims):
         batch_size = 2
+        extra_dim = [1]*dims
         model = MLP(depth=depth, width=width)
         rng = jax.random.PRNGKey(0)
-        inputs = np.random.RandomState(0).normal(size=[batch_size, in_size])
-        output, _ = model.init_with_output(rng, inputs)
-        self.assertEqual((batch_size, exp_width), output.shape)
 
-    @parameterized.parameters(
-        (1, 128, 16, 128),
-        (3, 128, 16, 128),
-        (0, 128, 16, 16)
-    )
-    def test_mlp_nd_returns_correct_output_shape(self, depth, width, in_size, exp_width):
-        batch_size = 2
-        model = MLP(depth=depth, width=width)
-        rng = jax.random.PRNGKey(0)
-        for n in [[], [1], [1,1]]:
-            inputs = jax.random.normal(rng, shape=[batch_size, *n, in_size])
-            output, _ = model.init_with_output(rng, inputs)
-            self.assertEqual((batch_size, *n, exp_width), output.shape)
+        inputs = jax.random.normal(rng, shape=[batch_size, *extra_dim, in_size])
+        output, pram = model.init_with_output(rng, inputs)
+        @self.variant
+        def run(pram, inputs):
+            return model.apply(pram, inputs)
+
+        res = run(pram, inputs)
+        self.assertEqual((batch_size, *extra_dim, exp_width), res.shape)
 
     def test_with_key_input(self):
         batch_size = 2
@@ -50,32 +47,28 @@ class MLPTest(parameterized.TestCase):
         self.assertEqual((batch_size, 128), output.shape)
 
 
-class MLCTest(parameterized.TestCase):
-    @parameterized.parameters(
-        (1, 128, 64),
-        (3, 128, 16),
-        (1, 16, 8)
+class MLCTest(chex.TestCase):
+    @chex.variants(with_jit=True, without_jit=True)
+    @parameterized.product(
+        (dict(depth=1, in_size=128, exp_width=64),
+         dict(depth=3, in_size=128, exp_width=16),
+         dict(depth=1, in_size=16, exp_width=8)
+         ),
+        dims=(1,2,3)
     )
-    def test_mlc_2d_returns_correct_output_shape(self, depth, in_size, exp_width):
+    def test_correct_output_shape(self, depth, in_size, exp_width, dims):
         batch_size = 2
-        model = MLC(depth=depth, features=16, kernel_size=(3,3), pool_size=(2,2))
+        model = MLC(depth=depth, features=16, kernel_size=(3,)*dims, pool_size=(2,)*dims)
         rng = jax.random.PRNGKey(0)
-        inputs = np.random.RandomState(0).normal(size=[batch_size, in_size, in_size, 1])
-        output, _ = model.init_with_output(rng, inputs)
-        self.assertEqual((batch_size, exp_width, exp_width, 16), output.shape)
+        inputs = np.random.RandomState(0).normal(size=[batch_size, *[in_size]*dims, 1])
+        output, pram = model.init_with_output(rng, inputs)
+        self.assertEqual((batch_size, *[exp_width]*dims, 16), output.shape)
+        @self.variant
+        def run(pram, inputs):
+            return model.apply(pram, inputs)
 
-    @parameterized.parameters(
-        (1, 128, 64),
-        (3, 128, 16),
-        (1, 16, 8)
-    )
-    def test_mlc_3d_returns_correct_output_shape(self, depth, in_size, exp_width):
-        batch_size = 2
-        model = MLC(depth=depth, features=16, kernel_size=(3,3,3), pool_size=(2,2,2))
-        rng = jax.random.PRNGKey(0)
-        inputs = np.random.RandomState(0).normal(size=[batch_size, in_size, in_size, in_size, 1])
-        output, _ = model.init_with_output(rng, inputs)
-        self.assertEqual((batch_size, exp_width, exp_width, exp_width, 16), output.shape)
+        res = run(pram, inputs)
+        self.assertEqual((batch_size, *[exp_width]*dims, 16), res.shape)
 
     def test_with_key_input(self):
         batch_size = 2
@@ -83,19 +76,6 @@ class MLCTest(parameterized.TestCase):
         rng = jax.random.PRNGKey(0)
         inputs = {"x": jax.random.normal(rng, shape=[batch_size, 128, 128, 1])}
         output, _ = model.init_with_output(rng, inputs)
-        self.assertEqual((batch_size, 16, 16, 16), output.shape)
-
-    def test_jit(self):
-        batch_size = 2
-        model = MLC(depth=3, features=16, kernel_size=(3, 3), pool_size=(2, 2), key="x")
-        rng = jax.random.PRNGKey(0)
-        inputs = {"x": jax.random.normal(rng, shape=[batch_size, 128, 128, 1])}
-        output, pram = model.init_with_output(rng, inputs)
-        @jax.jit
-        def run(pram, inputs):
-            return model.apply(pram, inputs)
-
-        res = run(pram, inputs)
         self.assertEqual((batch_size, 16, 16, 16), output.shape)
 
 
