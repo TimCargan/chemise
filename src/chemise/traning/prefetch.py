f7b822f1c5353812a7572d869a0476e36c33e103
Tim Cargan <timcargan@gmail.com>
2022-09-23 16:48:44 +0100

    Bufferd prefetch like the flax docs

--- src/chemise/traning/prefetch.py
+++ src/chemise/traning/prefetch.py
@@ -1,3 +1,5 @@
+import collections
+import itertools
 import queue
 from threading import Thread
 
@@ -17,11 +19,48 @@ class Prefetch_dev(Thread):
         super(Prefetch_dev, self).__init__()
         self.data = data
         self.q = queue.Queue(buffer_size)
+        self.buffer_size = buffer_size
         platform = jax.default_backend()
         d_count = jax.device_count(platform)
         logging.log_first_n(logging.INFO, "Running on %s with %d devices", 1, platform, d_count)
 
     def iter(self):
+        queue = collections.deque()
+        devices = jax.local_devices()
+        dev_count = len(devices)
+        def _prefetch(data, devs):
+            return jax.device_put_sharded(data, devs)
+
+        first = next(self.data)
+        batch_size = get_batch_size(first)
+        logging.debug("Sharded prefetch to %d devices, assumed new batch shape [%d, %d, ...]", len(devices),
+                      len(devices), batch_size)
+
+        shard_data = [first] + list(itertools.islice(self.data, dev_count - 1))
+        queue.append(_prefetch(shard_data, devices[:len(shard_data)]))
+
+        def enqueue(n):  # Enqueues *up to* `n` elements from the iterator.
+            for _ in range(n):
+                batch = list(itertools.islice(self.data, dev_count))
+                num_shards = len(batch)
+                if num_shards < 1:
+                    return
+                # 1 shard or n shard of all the same size
+                if num_shards == 1 or (num_shards > 1 and get_batch_size(batch[-1]) == batch_size):
+                    queue.append(_prefetch(batch, devices[: num_shards]))
+                    return
+                # n shards where we assume all but the last are the same
+                # (if this doesn't hold something funky happened batching)
+                n_shards, last_shard = batch[:-1], batch[-1:]
+                assert get_batch_size(n_shards[-1]) == batch_size, "Multiple batches of unequal size"
+                queue.append(_prefetch(n_shards, devices[: num_shards - 1]))
+                queue.append(_prefetch(last_shard, devices[: 1]))
+
+        enqueue(self.buffer_size - 1)  # Fill up the buffer, less the first already in.
+        while queue:
+            yield queue.popleft()
+            enqueue(1)
+    def iter_old(self):
         @jax.jit
         def _stack(flat):
             flat_n = [[n[l] for n in flat] for l in range(len(flat[0]))]
@@ -33,8 +72,7 @@ class Prefetch_dev(Thread):
             stacked = _stack(flat)
             uf = jax.tree_util.tree_unflatten(tree, stacked)
             return uf
-        def _prefetch(data, devs):
-            return jax.device_put_sharded(data, devs)
+
 
         devices = jax.local_devices()
         first = next(self.data)
