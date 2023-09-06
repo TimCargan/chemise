6b91678e02b59b63238a4cb94e03b7489305b417
Tim Cargan <timcargan@gmail.com>
2023-09-06 13:27:35 +0200

    add some basic data classes that look like tf.data so we can remove the dep / easy for testing
    Signed-off-by: Tim Cargan <timcargan@gmail.com>

---
+++ src/chemise/data.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import abc
+import dataclasses
+
+import jax
+import numpy as np
+
+
+@dataclasses.dataclass
+class Spec:
+    shape: tuple[..., int]
+    dtype: np.dtype
+
+
+class Data(abc.ABC):
+    @abc.abstractmethod
+    def cardinality(self) -> int:
+        """Return the cardinality of the data set, if unknown return -1."""
+
+    @property
+    @abc.abstractmethod
+    def element_spec(self) -> dict:
+        """Returns the shape of the data."""
+
+    @abc.abstractmethod
+    def as_numpy_iterator(self) -> iter:
+        """Return an iterator that yields numpy data"""
+
+
+class ListData(Data):
+    def __init__(self, data: list, add_batch_dim: bool = False):
+        self.data = data
+        self.add_batch_dim = add_batch_dim
+
+    def cardinality(self) -> int:
+        return len(self.data)
+
+    @property
+    def element_spec(self):
+        def make_spec(el):
+            """"""
+            shape = (None, *el.shape[:1]) if self.add_batch_dim else(*el.shape[:1], )
+            shape = (*shape, *el.shape[1:])
+            return Spec(shape=shape, dtype=el.dtype)
+        return jax.tree_util.tree_map(make_spec, self.data[0])
+
+    def take(self, n: int) -> ListData:
+        return ListData(self.data[:n], self.add_batch_dim)
+
+    def as_numpy_iterator(self):
+        for x in self.data:
+            yield x
+
+    def map(self, f, *args, **kwargs):
+        print("Warning: you called map")
+        return ListData(list(map(f, self.data)), self.add_batch_dim)
