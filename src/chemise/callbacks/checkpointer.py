5ef7e8526e2f618cf5bd877e18ce6894072cd9c2
Tim Cargan <timcargan@gmail.com>
2023-11-22 10:51:40 +0000

    Update checkpointer.py to save the model at the end of every epoch in its own namespace
    Signed-off-by: Tim Cargan <timcargan@gmail.com>

--- src/chemise/callbacks/checkpointer.py
+++ src/chemise/callbacks/checkpointer.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 import datetime
-import orbax
-from absl import logging
 from dataclasses import dataclass
-from flax.training import orbax_utils
-from jax import numpy as jnp
 from pathlib import Path
 from typing import TYPE_CHECKING
+
+import orbax.checkpoint
+from absl import logging
+from flax.training import orbax_utils
+from jax import numpy as jnp
 
 from chemise.callbacks.abc_callback import Callback
 
@@ -21,19 +22,21 @@ class Checkpointer(Callback):
     keep: int = 1
     overwrite: bool = False
     keep_every_n_steps: int = None
-    intra_train_freq: int = None
-    auto_restore: bool = False
     keep_time_interval: datetime.timedelta = None
+    intra_train_freq: int = None
+    auto_restore: bool = False  # Restore the most recent ckpt when training begins
+    keep_epoch_steps: bool = False  # Keep the step number for every epoch
+    save_epochs: bool = True  # Save epochs as their own checkpoints with a prefix epoch
     _step_count: int = 0
+    _epoch: int = 0
     _save_args = None  # A mapping to let the checkpoint manager know how to compress the ckpt
 
     def __post_init__(self):
         mgr_options = orbax.checkpoint.CheckpointManagerOptions(
             create=True, max_to_keep=self.keep, keep_time_interval=self.keep_time_interval,
             keep_period=self.keep_every_n_steps, step_prefix='ckpt')
-        self.ckpt_mgr = orbax.checkpoint.CheckpointManager(self.ckpt_dir,
-                                                           orbax.checkpoint.Checkpointer(
-                                                               orbax.checkpoint.PyTreeCheckpointHandler()), mgr_options)
+        self.checkpointer = orbax.checkpoint.Checkpointer(orbax.checkpoint.PyTreeCheckpointHandler())
+        self.ckpt_mgr = orbax.checkpoint.CheckpointManager(self.ckpt_dir, self.checkpointer, mgr_options)
 
     def set_step_number(self, step: int):
         self._step_count = step
@@ -45,25 +48,29 @@ class Checkpointer(Callback):
             logging.warning("Restoring checkpoint at start of run")
             step = self.ckpt_mgr.latest_step()
             trainer.state = self.ckpt_mgr.restore(step, items=trainer.state)
-            # trainer.state = cp.restore_checkpoint(self.ckpt_dir, trainer.state)
+            self.set_step_number(int(jnp.max(trainer.state.step)))
 
     def on_train_batch_end(self, trainer: BasicTrainer):
-        self._step_count += 1
+        self._step_count += 1 # Use of an internal step count to Dev to Host call
         if self.intra_train_freq and self._step_count % self.intra_train_freq == 0:
             self.save(trainer)
 
     def on_epoch_end(self, trainer: BasicTrainer):
-        self.save(trainer)
+        # This is a hack and I should find a better way
+        if self.keep_epoch_steps:
+            step = int(jnp.max(trainer.state.step))
+            self.ckpt_mgr._options.save_on_steps.append(step)
 
-    def save(self, trainer: BasicTrainer):
-        # Need to find out what this does
+        if self.save_epochs:
+            mgr_options = orbax.checkpoint.CheckpointManagerOptions(create=True, step_prefix='epoch')
+            epoch_ckpt_mgr = orbax.checkpoint.CheckpointManager(self.ckpt_dir, self.checkpointer, mgr_options)
+            epoch_ckpt_mgr.save(self._epoch, trainer.state, save_kwargs={'save_args': self._save_args}, force=True)
+
+        self._epoch += 1
+
+    def save(self, trainer: BasicTrainer, force: bool = False):
         step = int(jnp.max(trainer.state.step))
-        self.ckpt_mgr.save(step, trainer.state, save_kwargs={'save_args': self._save_args})
-        # orbax_checkpointer = None #orbax.Checkpointer(orbax.PyTreeCheckpointHandler())
-        # cp.save_checkpoint(target=trainer.state, step=trainer.state.step,
-        #                    ckpt_dir=self.ckpt_dir, overwrite=self.overwrite,
-        #                    keep=self.keep, keep_every_n_steps=self.keep_every_n_steps,
-        #                    orbax_checkpointer=orbax_checkpointer)
+        self.ckpt_mgr.save(step, trainer.state, save_kwargs={'save_args': self._save_args}, force=force)
 
     @staticmethod
     def restore(trainer: BasicTrainer, ckpt_dir: Path | str, step_prefix: str = "ckpt", use_restore_kwargs: bool = True):
@@ -74,11 +81,8 @@ class Checkpointer(Callback):
         ckpt_mgr = orbax.checkpoint.CheckpointManager(ckpt_dir, ckpter, mgr_options)
 
         step = ckpt_mgr.latest_step()
-        # TDOD: find out more about the kwargs
+        # TODO: find out more about the kwargs
         restore_args = orbax_utils.restore_args_from_target(trainer.state, mesh=None)
         restore_kwargs = {'restore_args': restore_args} if use_restore_kwargs else None
         trainer.state = ckpt_mgr.restore(step, items=trainer.state, restore_kwargs=restore_kwargs)
-        # orbax_checkpointer = None #orbax.Checkpointer(orbax.PyTreeCheckpointHandler())
-        # trainer.state = cp.restore_checkpoint(ckpt_dir=ckpt_dir, target=trainer.state,
-        #                                       orbax_checkpointer=orbax_checkpointer)
         return trainer
