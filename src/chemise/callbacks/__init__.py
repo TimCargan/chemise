b47a3c8c92ef1ff9cdd206b4167921b09274cc37
Tim Cargan <timcargan@gmail.com>
2024-04-05 18:20:38 +0100

    Nicer imports of KeepBest and base Callback
    Signed-off-by: Tim Cargan <timcargan@gmail.com>

--- src/chemise/callbacks/__init__.py
+++ src/chemise/callbacks/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
+
+from chemise.callbacks.abc_callback import Callback
 from chemise.callbacks.checkpointer import Checkpointer
-from chemise.callbacks.progress_bar import ProgressBar
-from chemise.callbacks.profile import Profile
 from chemise.callbacks.grapher import Line
+from chemise.callbacks.keep_best import KeepBest
+from chemise.callbacks.profile import Profile
+from chemise.callbacks.progress_bar import ProgressBar
